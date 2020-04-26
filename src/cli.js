e76a3262363de8eb28a7e6f804ff32d42c4d9b24
katada93 <tsokaev93@mail.ru>
2020-04-27 00:45:43 +0300

    fix syntax errors

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const printName = () => {
+const printName = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
+
+export default printName;
