2763ec8cb1593a8bc548c254743cb17d24dea13d
katada93 <tsokaev93@mail.ru>
2020-04-27 00:22:58 +0300

    add eslint and fix some syntax errors

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
-import { printName } from "../src/cli.js";
-console.log("Welcome to the Brain Games!");
+import { printName } from '../src/cli.js';
+
+console.log('Welcome to the Brain Games!');
 printName();
